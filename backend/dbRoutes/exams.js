c628955f1c52512cffa38dba93381cf9c072ba90
MS14-dev <mshamalka2@gmail.com>
2022-01-21 02:13:17 +0530

    New course adding function done

--- backend/dbRoutes/exams.js
+++ backend/dbRoutes/exams.js
@@ -14,6 +14,26 @@ const findExamsByCourseId=async(courseId)=>{
 
 }
 
+const addNewExam =(id,course,t1,t2,exam1,exam2)=>{
+    return new Promise((resolve,reject)=>{
+        
+        // let {wrong11,wrong12,wrong13,correct1} = ans1
+        // let {wrong21,wrong22,wrong23,correct2} = ans2
+        exam1Json = JSON.stringify(exam1)
+        exam2Json = JSON.stringify(exam2)
+
+        database.query(`insert into exams (id,course,theory1,theory2,exam1,exam2) 
+        values('${id}','${course}','${t1}','${t2}', ?,?)`,[exam1Json,exam2Json],(err,row)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(row)
+            }
+        })
+
+    })
+}
+
 const updateResultByConductId=async(conductId,examNumb)=>{
     return new Promise((resolve,reject)=>{
         database.query(`UPDATE results SET exam${examNumb} = 10 WHERE conduct = '${conductId}'`,(err,row)=>{
@@ -29,4 +49,14 @@ const updateResultByConductId=async(conductId,examNumb)=>{
 module.exports = {
     findExamsByCourseId,
     updateResultByConductId,
+    addNewExam
 }
+
+// `insert into exams (id,course,theory1,theory2,exam1,exam2) 
+//         values('${id}','${course}','${t1}','${t2}', 
+//         '{'question':'${q1}',
+//         'answers':'['{'answer':'${wrong11}','correct':'false'}','{'answer':'${wrong12}','correct':'false'}','{'answer':'${wrong13}','correct':'false'}','{'answer':'${correct1}','correct':'true'}']'}'
+//         ,
+//         '{'question':'${q2}',
+//         'answers':'['{'answer':'${wrong21}','correct':'false'}','{'answer':'${wrong22}','correct':'false'}','{'answer':'${wrong23}','correct':'false'}','{'answer':'${correct2}','correct':'true'}']'}'
+//         )`
--- backend/routes/institutionsRouter.js
+++ backend/routes/institutionsRouter.js
@@ -5,6 +5,8 @@ const fileUpload = require('express-fileupload')
 
 const {addNewInstitution,findInstitutionByUserName,findInstitutionById} = require('../dbRoutes/institutions');
 const {addNewCourse} = require('../dbRoutes/courses')
+const {addNewExam} = require('../dbRoutes/exams');
+const { response } = require('express');
 
 const institutionRouter = express.Router();
 institutionRouter.use(fileUpload());
@@ -74,35 +76,73 @@ institutionRouter.get('/logout',(req,res)=>{
 
 //for after login purposes
 institutionRouter.get('/by-id',async(req,res)=>{
+
+    if(req.session.isLogged){
     let institutionId = req.session.institutionId;
     let data = await findInstitutionById(institutionId);
+        if(data.length != 0){
+            res.send({response:true,message:'Successful',data:data[0]})
+        }else{
+            res.send({response:false,message:'Ooops some server error',data:null})
+        }
+    }
+    else{
+        res.send({response:false,message:'Need to login first',data:null})
+    }
 })
 
 institutionRouter.post('/add-new-course',async(req,res)=>{
-    let {name,description} = req.body
+    let {name,
+        description,
+        theory1,
+        theory2,
+        question1,
+        question2,
+        wrong11,
+        wrong12,
+        wrong13,
+        wrong21,
+        wrong22,
+        wrong23,
+        correct1,
+        correct2} = req.body
     let image = req.files.image;
+    
+    if(req.session.isLogged){
+        let randomCourseId = uuid();
 
-    let randomCourseId = uuid();
+        //this will be catch via session, in here a demostration only
+        let innstitutionId = req.session.institutionId;
 
-    //this will be catch via session, in here a demostration only
-    let innstitutionId = "68a5c3e2-f225-45f1-947a-00ed5ce6d9ad";
+        let newFilePath = __dirname+'/../public/images/courses/';
+        let newFileName = `${randomCourseId}` + image.name;
 
-    let newFilePath = __dirname+'/../public/images/courses/';
-    let newFileName = `${randomCourseId}` + image.name;
-
-    image.mv(`${newFilePath}${newFileName}`,async(err)=>{
-        if(err){
-            console.log('Sorry')
-            res.send({response:false,message:'Failed to add new course'})
-        }else{
-            let imagePath = `http://localhost:8000/public/images/courses/${newFileName}`
-            let data  = await addNewCourse(randomCourseId,name,innstitutionId,description,imagePath);
-            console.log(data)
-            // console.log('Success')
-            res.send({response:true,message:'Successfully Added'})
-        }
-    })
-
+        image.mv(`${newFilePath}${newFileName}`,async(err)=>{
+            if(err){
+                console.log('Sorry')
+                res.send({response:false,message:'Failed to add new course'})
+            }else{
+                let imagePath = `http://localhost:8000/public/images/courses/${newFileName}`
+                let data  = await addNewCourse(randomCourseId,name,innstitutionId,description,imagePath);
+                if(data.affectedRows == 1){
+                    
+                    let randomExamId = uuid();
+                    let exam1 = {question:question1,answers:[{answer:wrong11,correct:'false'},{answer:wrong12,correct:'false'},{answer:wrong13,correct:'false'},{answer:correct1,correct:'true'},]}
+                    let exam2 = {question:question2,answers:[{answer:wrong21,correct:'false'},{answer:wrong22,correct:'false'},{answer:wrong23,correct:'false'},{answer:correct2,correct:'true'},]}
+                    
+                    let afterAddExam = await addNewExam(randomExamId,randomCourseId,theory1,theory2,exam1,exam2)
+                    if(afterAddExam.affectedRows == 1){
+                        res.send({response:true,message:'Successfuly Added',data:afterAddExam})
+                    }
+                }
+                else{
+                res.send({response:false,message:'Oops some server error'})
+                }
+            }
+        })
+    }else{
+        res.send({response:false,message:'Need login first',data:null})
+    }
 })
 
 module.exports = institutionRouter;
